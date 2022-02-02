521fd9312dabc46c77019ebbfe79972294283652
xuatz <xzlow10@gmail.com>
2022-02-02 22:12:12 +0900

    chore: revert my own changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
+// This enables ESLint to use dependencies of this config
+// (see https://github.com/eslint/eslint/issues/3458)
+require('eslint-config-molindo/setupPlugins');
+
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:node/recommended"
-  ],
+  extends: 'molindo/javascript',
   env: {
     node: true,
     jest: true
